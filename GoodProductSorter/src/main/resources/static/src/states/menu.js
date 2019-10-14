2136ae7a65ab7ca05dfa561cdba3362cfcf514d7
Adrián Poza <adripoza23@gmail.com>
2019-10-15 00:21:40 +0200

    cosas mosca y animación caja

--- GoodProductSorter/src/main/resources/static/src/states/menu.js
+++ GoodProductSorter/src/main/resources/static/src/states/menu.js
@@ -49,8 +49,7 @@ GoodProductSorter.menuState.prototype ={
 		this.mosca.width = game.world._width;
 		this.mosca.height = game.world._height * 0.5;
 		this.mosca.animations.add('fly');
-		this.mosca.animations.play('fly', 10, false);		
-		game.time.events.repeat(Phaser.Timer.SECOND * 6, 10, this.playMosca, this);
+		game.time.events.repeat(Phaser.Timer.SECOND * 5, 10, this.playMosca, this);
 		
 		//Texto botones y Boton selector de idioma
 		if(this.game.global.IDIOMA=='ESP'){
